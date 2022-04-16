a3febbecb669f5aea286b801e63bdd8d858826c0
tuguldur <gtuguldur@iMac-22s-iMac.local>
2022-04-16 12:57:02 +0800

    yanzal

--- engine.js
+++ engine.js
@@ -10,22 +10,20 @@ function moveSelection(evt) {
 const gameOver = document.getElementById('endGame');
 const blueJeep = document.getElementById('blueJeep');
 element = document.getElementById("hun");
-function move(evt) {
 
+function move(evt) {
     if (evt.keyCode == 65) {
         // LEFT
         if (element.style.left >= 15 + '%') {
             element.style.left = (parseInt(element.style.left) - 1) + '%';
         }
         element.style.transform = 'scaleX(-1)'
-
     }
     if (evt.keyCode == 68) {
         // RIGHT
         if (element.style.left <= 94 + '%') {
             element.style.left = (parseInt(element.style.left) + 1) + '%';
         }
-
         element.style.transform = 'scaleX(1)'
     }
     if (evt.keyCode == 83) {
@@ -42,14 +40,14 @@ function move(evt) {
     }
 
 }
-function endGame(){
-    gameOver.classList.add('gameOver');
-    gameOver.innerHTML = 'GAME OVER';
-    element.style.transform = 'rotate(90deg)';
-    element.style.transform = 'scaleX(-1)';
-    blueJeep.style.animationPlayState = 'paused';
-    return;
-}
+// function endGame(){
+//     gameOver.classList.add('gameOver');
+//     gameOver.innerHTML = 'GAME OVER';
+//     element.style.transform = 'rotate(90deg)';
+//     element.style.transform = 'scaleX(-1)';
+//     blueJeep.style.animationPlayState = 'paused';
+//     return;
+// }
 function murguh(){
     if(blueJeep.getBoundingClientRect().bottom >= element.getBoundingClientRect().top && blueJeep.getBoundingClientRect().top <= element.getBoundingClientRect().bottom){
         if(blueJeep.getBoundingClientRect().right >= element.getBoundingClientRect().left && blueJeep.getBoundingClientRect().left <= element.getBoundingClientRect().right) {
