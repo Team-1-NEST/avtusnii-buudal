6cc67f2908e72aa4f29905ca75027797c90f9ef4
Sikk10 <tuguldurganbayar1014@gmail.com>
2022-04-20 23:42:36 +0800

    gertee
    gertee

--- engine.js
+++ engine.js
@@ -1,63 +1,48 @@
 var element;
 var inter;
+
+const gameOver = document.getElementById('endGame');
+const blueJeep = document.getElementById('blueJeep');
+const hun = document.getElementById("hun");
+const area = document.getElementsByClassName("area")[0];
+
+//HUDULGUDUG FUNKTSUUD
+
 function moveSelection(evt) {
     if (inter) {
         clearTimeout(inter);
     }
-
     inter = setInterval(move, 20, evt)
 }
-const gameOver = document.getElementById('endGame');
-const blueJeep = document.getElementById('blueJeep');
-element = document.getElementById("hun");
 
 function move(evt) {
-    if (evt.keyCode == 65) {
+    if (evt.keyCode == 65 || evt.keyCode == 37) {
         // LEFT
-        if (element.style.left >= 15 + '%') {
-            element.style.left = (parseInt(element.style.left) - 1) + '%';
+        if (hun.style.left > 0 + '%') {
+            hun.style.left = (parseFloat(hun.style.left) - 0.5) + '%';
         }
-        element.style.transform = 'scaleX(-1)'
+        hun.style.transform = 'scaleX(-1)'
     }
-    if (evt.keyCode == 68) {
+    if (evt.keyCode == 68 || evt.keyCode == 39) {
         // RIGHT
-        if (element.style.left <= 94 + '%') {
-            element.style.left = (parseInt(element.style.left) + 1) + '%';
+        if (hun.style.left < 93 + '%') {
+            hun.style.left = (parseFloat(hun.style.left) + 0.5) + '%';
         }
-        element.style.transform = 'scaleX(1)'
+        hun.style.transform = 'scaleX(1)'
     }
-    if (evt.keyCode == 83) {
+    if (evt.keyCode == 83 || evt.keyCode == 40) {
         // DOWN
-        if (element.style.top <= 80 + '%') {
-            element.style.top = (parseInt(element.style.top) + 1) + '%';
+        if (hun.style.top < 85 + '%'){
+            hun.style.top = (parseFloat(hun.style.top) + 0.5) + '%';
         }
     }
-    if (evt.keyCode == 87) {
+    if (evt.keyCode == 87 || evt.keyCode == 38) {
         // UP
-        if (element.style.top >= 43 + '%') {
-            element.style.top = (parseInt(element.style.top) - 1) + '%';
+        if (hun.style.top > 49 + '%') {
+            hun.style.top = (parseFloat(hun.style.top) - 0.5) + '%';
         }
     }
-
 }
-// function endGame(){
-//     gameOver.classList.add('gameOver');
-//     gameOver.innerHTML = 'GAME OVER';
-//     element.style.transform = 'rotate(90deg)';
-//     element.style.transform = 'scaleX(-1)';
-//     blueJeep.style.animationPlayState = 'paused';
-//     return;
-// }
-function murguh(){
-    if(blueJeep.getBoundingClientRect().bottom >= element.getBoundingClientRect().top && blueJeep.getBoundingClientRect().top <= element.getBoundingClientRect().bottom){
-        if(blueJeep.getBoundingClientRect().right >= element.getBoundingClientRect().left && blueJeep.getBoundingClientRect().left <= element.getBoundingClientRect().right) {
-            endGame();
-        }
-    }
-    
-};
-
-setInterval( murguh, 0.1)
 
 function moving(evt) {
     clearInterval(inter)
@@ -67,6 +52,31 @@ function moving(evt) {
 window.addEventListener('keydown', moveSelection);
 window.addEventListener('keyup', moving);
 
-function trafficLight() {
-    
+// DARAACHIIN GAZAR SONGOH
+
+function nextMove(){
+    if(area.getBoundingClientRect().left <= hun.getBoundingClientRect().right && area.getBoundingClientRect().right >= hun.getBoundingClientRect().left && area.getBoundingClientRect().bottom >= hun.getBoundingClientRect().bottom){
+        endGame();
+    }
 }
+
+setInterval(nextMove, 0.1)
+
+function endGame(){
+    gameOver.classList.add('gameOver');
+    gameOver.innerHTML = 'GAME OVER';
+    // element.style.transform = 'rotate(90deg)';
+    // element.style.transform = 'scaleX(-1)';
+    // blueJeep.style.animationPlayState = 'paused';
+    return;
+}
+function murguh(){
+    if(blueJeep.getBoundingClientRect().bottom >= element.getBoundingClientRect().top && blueJeep.getBoundingClientRect().top <= element.getBoundingClientRect().bottom){
+        if(blueJeep.getBoundingClientRect().right >= element.getBoundingClientRect().left && blueJeep.getBoundingClientRect().left <= element.getBoundingClientRect().right) {
+            endGame();
+        }
+    }
+    
+};
+
+//setInterval( murguh, 0.1)
